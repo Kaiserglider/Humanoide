6912cfb4a6d72d4570849f89af799208638cf90d
Jose Pablo Lopez <101880117+Kaiserglider@users.noreply.github.com>
2024-10-23 10:24:56 -0700

    Update Control100924.ino

--- Control100924.ino
+++ Control100924.ino
@@ -62,7 +62,7 @@ int angleToPulse(int ang) {
 
 void setInitialServoPositions() {
     for (int i = 0; i < 14; i++) {
-        pwm.setPWM(i, 0, angleToPulse(posiciones[i])); // Utiliza el array posiciones
+        pwm.setPWM(i, 0, angleToPulse(posiciones[i])); //Utiliza el array posiciones
     }
 }
 
@@ -72,9 +72,6 @@ void smoothMove(int count, int servos[], int startAngles[], int endAngles[], int
   int pulsesStart[count];
   int pulsesEnd[count];
   int pulseSteps[count];
-
-  //Numero de pasos, puedes ajustar según lo que necesites
-  const int steps = 100; // Asegúrate de definirlo o pasar como argumento
 
   //Calcula el pulso inicial, final y paso para cada servo
   for (int i = 0; i < count; i++) {
@@ -194,27 +191,27 @@ void sentar(){
 }
 void izquierda(){
     int servos06[] = { 4, 5, 0, 6};
-    int startAngles06[] = {posiciones[4],posiciones[5], posiciones[0], posiciones[6]};
+    int startAngles06[] = {130, 95, 28, 20};
     int endAngles06[] = { 160, 65, 20, 45};
     smoothMove(4, servos06, startAngles06, endAngles06, t01);
 
     int servos07[] = { 5, 2, 3 };
-    int startAngles07[] = {posiciones[5], posiciones[2], posiciones[3]};
+    int startAngles07[] = {65, 93, 155};
     int endAngles07[] = { 45, 98, 145};
     smoothMove(3, servos07, startAngles07, endAngles07, t01);
 
     int servos08[] = { 4, 3};
-    int startAngles08[] = {posiciones[4], posiciones[3]};
+    int startAngles08[] = {155, 145};
     int endAngles08[] = { 135, 130};
     smoothMove(2, servos08, startAngles08, endAngles08, t01);
 
     int servos09[] = { 5, 3, 0 };
-    int startAngles09[] = {posiciones[5],posiciones[3],posiciones[0]};
+    int startAngles09[] = {45, 130, 20};
     int endAngles09[] = { 55, 140, 15 };
     smoothMove(3, servos09, startAngles09, endAngles09, t03);
 
     int servos10[] = { 5, 3, 4, 2, 0, 6};
-    int startAngles10[] = {posiciones[5], posiciones[3]};
+    int startAngles10[] = {55, 140, 135, 98, 15, 45};
     int endAngles10[] = { 85, 155, 130, 93, 35, 0};
     smoothMove(6, servos10, startAngles10, endAngles10,t03);
     delay(20);
@@ -225,54 +222,54 @@ void izquierda(){
 
 void derecha () {
     int servos01[] = { 1, 2, 3, 7 };
-    int startAngles01[] = {posiciones[1], posiciones[2], posiciones[3], posiciones[7]};
+    int startAngles01[] = {55, 93, 155, 160};
     int endAngles01[] = { 25, 123, 160, 155 };
     smoothMove(4, servos01, startAngles01, endAngles01, t01);
 
     int servos02[] = { 2, 5, 0, 6};
-    int startAngles02[] = {posiciones[2], posiciones[5], posiciones[0], posiciones[6]};
+    int startAngles02[] = {123, 95, 28, 20};
     int endAngles02[] = { 143, 90, 35, 90};
     smoothMove(4, servos02, startAngles02, endAngles02, t01);
 
     int servos03[] = { 1, 0, 6};
-    int startAngles03[] = {posiciones[1], posiciones[0], posiciones[6]};
+    int startAngles03[] = {25, 35, 90};
     int endAngles03[] = { 45, 50, 70};
     smoothMove(3, servos03, startAngles03, endAngles03, t01);
 
     int servos04[] = { 2, 0, 3, 6};
-    int startAngles04[] = {posiciones[2], posiciones[0], posiciones[3], posiciones[6]};
+    int startAngles04[] = {143, 50, 160, 70};
     int endAngles04[] = { 133, 50, 165, 50};
     smoothMove(4, servos04, startAngles04, endAngles04, t02);
 
     int servos05[] =      {2, 0, 1, 5, 3, 7, 6};
-    int startAngles05[] = {posiciones[2], posiciones[0], posiciones[1], posiciones[5], posiciones[3], posiciones[7], posiciones[6]};
+    int startAngles05[] = {123, 50, 45, 90, 165, 155, 50};
     int endAngles05[] =   {93, 28, 50, 95, 155, 160, 20};
     smoothMove(7, servos05, startAngles05, endAngles05, t02);
 }
 
 void girar () {
     int servos01[] = { 1, 2, 3, 7 };
-    int startAngles01[] = {posiciones[1], posiciones[2], posiciones[3], posiciones[7]};
+    int startAngles01[] = {55, 93, 155, 160};
     int endAngles01[] = { 25, 123, 160, 155 };
     smoothMove(4, servos01, startAngles01, endAngles01, t01);
 
     int servos02[] = { 2, 5, 0, 6};
-    int startAngles02[] = {posiciones[2], posiciones[5], posiciones[0], posiciones[6]};
+    int startAngles02[] = {123, 95, 28, 20};
     int endAngles02[] = { 143, 90, 35, 90};
     smoothMove(4, servos02, startAngles02, endAngles02, t01);
 
     int servos03[] = { 1, 0, 6};
-    int startAngles03[] = {posiciones[1], posiciones[0], posiciones[6]};
+    int startAngles03[] = {25, 35, 90};
     int endAngles03[] = { 45, 50, 70};
     smoothMove(3, servos03, startAngles03, endAngles03, t01);
 
     int servos04[] = { 2, 0, 3, 6};
-    int startAngles04[] = {posiciones[2], posiciones[0], posiciones[3], posiciones[6]};
+    int startAngles04[] = {143, 50, 160, 70};
     int endAngles04[] = { 133, 50, 165, 50};
     smoothMove(4, servos04, startAngles04, endAngles04, 15);
 
     int servos05[] =      {2, 0, 1, 5, 3, 7, 6};
-    int startAngles05[] = {posiciones[2], posiciones[0], posiciones[1], posiciones[5], posiciones[3], posiciones[7], posiciones[6]};
+    int startAngles05[] = {123, 50, 45, 90, 165, 155, 50};
     int endAngles05[] =   {93, 28, 50, 95, 155, 160, 20};
     smoothMove(7, servos05, startAngles05, endAngles05, 15);
 }

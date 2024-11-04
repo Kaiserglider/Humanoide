595c63c314749345cac834a07fdd957345b29846
RvillaMedina <95550220+RvillaMedina@users.noreply.github.com>
2024-11-03 18:38:08 -0700

    Update Control100924.ino

--- Control100924.ino
+++ Control100924.ino
@@ -15,7 +15,9 @@
 
 #define stepClearance 2
 #define stepHeight 16
-
+int k01=10;
+int k02=10;
+int k03=10;
 /*#include <SPI.h>
 #include <SD.h>
 
@@ -47,7 +49,7 @@ int servos[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13};
 int CAM=13;
 //Definir tiempos
 int t01 = 20;
-int t02 = 30;
+int t02 = 10;
 int t03 = 35;     // Tiempo de retraso entre movimientos
 int steps = 10;  // Número de pasos para suavizar el movimiento
 //Variables para control manual de motores
@@ -130,23 +132,7 @@ int angleToPulse(int ang) {
 }
 
 void setInitialServoPositions() {
-    for (int i = 0; i < 14; i++) {
-        pwm.setPWM(i, 0, angleToPulse(posiciones[i])); //Utiliza el array posiciones
-    }
-    P0=posiciones[0];
-    P1=posiciones[1];
-    P2=posiciones[2];
-    P3=posiciones[3];
-    P4=posiciones[4];
-    P5=posiciones[5];
-    P6=posiciones[6];
-    P7=posiciones[7];
-    P8=posiciones[8];
-    P9=posiciones[9];
-    P10=posiciones[10];
-    P11=posiciones[11];
-    P12=posiciones[12];
-    P13=posiciones[13];
+   AdvMoveAbs(100,10,posiciones[0],posiciones[1],posiciones[2],posiciones[3],posiciones[4],posiciones[5],posiciones[6],posiciones[7],posiciones[8],posiciones[9],posiciones[10],posiciones[11],posiciones[12],posiciones[13]);
 }
 
 //Funcion de movimiento
@@ -176,13 +162,13 @@ void smoothMove(int count, int servos[], int startAngles[], int endAngles[], int
 void AdvMoveAbs(int time, int steps,int X0,int X1,int X2,int X3,int X4,int X5,int X6,int X7,int X8,int X9,int X10,int X11,int X12,int X13) {
   int AbsAngles[]= {X0,X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12,X13};
   int startAngles[]={P0,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13};
-//  Serial.print(X0); //Diagnostic Mode
-//  Serial.println();
+/*  Serial.print(X0); //Diagnostic Mode
+  Serial.println();
   for (int i = 0; i < CAM; i++) {
     Serial.print(startAngles[i]);
     Serial.write("\t");
   }
-  Serial.println();
+  Serial.println();*/
   smoothMove(14, servos, startAngles, AbsAngles, time);
  // Array de Pulsos Iniciales y Finales
     P0=X0;
@@ -288,7 +274,30 @@ void processCommand(String command) {
     }
     return;
   }
-
+if (command.startsWith("K1:")) {
+    int newValue = command.substring(3).toInt(); // Extraer el valor 
+    if (newValue > 0) {
+      k01 = newValue; // Actualizar el valor
+    } else {
+    }
+    return;
+  }
+ if (command.startsWith("K2:")) {
+    int newValue = command.substring(3).toInt(); // Extraer el valor
+    if (newValue > 0) {
+      k02 = newValue; // Actualizar el valor
+    } else {
+    }
+    return;
+  }
+   if (command.startsWith("K3:")) {
+    int newValue = command.substring(3).toInt(); // Extraer el valor
+    if (newValue > 0) {
+      k03 = newValue; // Actualizar el valor
+    } else {
+    }
+    return;
+  }
 // modo manual para mover los servos
   if (command.startsWith("M")) {
     int nV = command.substring(1).toInt(); // Extrae
@@ -328,25 +337,20 @@ if (command.startsWith("caminar")) {
     delay(1000);
     derecha2();
     delay(1000);
-    izquierda();
     delay(10);
 }
 if (command.startsWith("cinematic")) {
   //cinematica
-    takeStep(5, 0);
+    takeStep(5, t01);
 }
 if (command.startsWith("prepare")) {
   //cinematica
     initialize();
 }
 if (command.startsWith("derecha")) {
-    derecha2();
+   derecha2();
 }
-if (command.startsWith("girar")) {
-    girar();
 }
-if (command.startsWith("sentar")) {
-    sentar();
 /* 
    delay (5000);
    int servos02[] = { 0, 1, 2, 3, 4, 5, 6, 7,8,9, 10, 11, 12, 13};
@@ -354,74 +358,10 @@ if (command.startsWith("sentar")) {
    int endAngles02[] = {V0, V1, V2, V3, V4, V5 ,V6, V7, V8, V9, V10, V11, V12, V13};
    smoothMove(14, servos02, startAngles02, endAngles02, 30);
 */
-}
 
-}
+
+
 //Secuencias de Movimiento
-void sentar(){
-  delay(1000);
-    int servos00[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-    int startAngles00[] = {posiciones[0], posiciones[1], posiciones[2], posiciones[3], posiciones[4], posiciones[5], posiciones[6], posiciones[7], posiciones[8], posiciones[9], posiciones[10], posiciones[11], posiciones[12], posiciones[13]};
-    int endAngles00[] = { 25, 30, 180, 165, 160, 5, 1, 179, 135, 70, 90, 45, 110, 90 };
-    smoothMove(14, servos00, startAngles00, endAngles00, t01);
-}
-void izquierda(){
-    int servos06[] = { 4, 5, 0, 6};
-    int startAngles06[] = {130, 95, 28, 20};
-    int endAngles06[] = { 160, 65, 20, 45};
-    smoothMove(4, servos06, startAngles06, endAngles06, t01);
-
-    int servos07[] = { 5, 2, 3 };
-    int startAngles07[] = {65, 93, 155};
-    int endAngles07[] = { 45, 98, 145};
-    smoothMove(3, servos07, startAngles07, endAngles07, t01);
-
-    int servos08[] = { 4, 3};
-    int startAngles08[] = {155, 145};
-    int endAngles08[] = { 135, 130};
-    smoothMove(2, servos08, startAngles08, endAngles08, t01);
-
-    int servos09[] = { 5, 3, 0 };
-    int startAngles09[] = {45, 130, 20};
-    int endAngles09[] = { 55, 140, 15 };
-    smoothMove(3, servos09, startAngles09, endAngles09, t03);
-
-    int servos10[] = { 5, 3, 4, 2, 0, 6};
-    int startAngles10[] = {55, 140, 135, 98, 15, 45};
-    int endAngles10[] = { 85, 155, 130, 93, 35, 0};
-    smoothMove(6, servos10, startAngles10, endAngles10,t03);
-    delay(20);
-    pwm.setPWM(0, 0, angleToPulse(posiciones[1]));
-    delay(200);
-
-}
-
-void derecha () {
-    int servos01[] = { 1, 2, 3, 7 };
-    int startAngles01[] = {55, 93, 155, 160};
-    int endAngles01[] = { 25, 123, 160, 155 };
-    smoothMove(4, servos01, startAngles01, endAngles01, t01);
-
-    int servos02[] = { 2, 5, 0, 6};
-    int startAngles02[] = {123, 95, 28, 20};
-    int endAngles02[] = { 143, 90, 35, 90};
-    smoothMove(4, servos02, startAngles02, endAngles02, t01);
-
-    int servos03[] = { 1, 0, 6};
-    int startAngles03[] = {25, 35, 90};
-    int endAngles03[] = { 45, 50, 70};
-    smoothMove(3, servos03, startAngles03, endAngles03, t01);
-
-    int servos04[] = { 2, 0, 3, 6};
-    int startAngles04[] = {143, 50, 160, 70};
-    int endAngles04[] = { 133, 50, 165, 50};
-    smoothMove(4, servos04, startAngles04, endAngles04, t02);
-
-    int servos05[] =      {2, 0, 1, 5, 3, 7, 6};
-    int startAngles05[] = {123, 50, 45, 90, 165, 155, 50};
-    int endAngles05[] =   {93, 28, 50, 95, 155, 160, 20};
-    smoothMove(7, servos05, startAngles05, endAngles05, t02);
-}
 
 void derecha2 () {
 AdvMoveRel(20,10,0,-30,30,5,0,0,0,-5,0,0,0,0,0,0);
@@ -432,39 +372,14 @@ AdvMoveRel(20,10,15,20,0,0,0,0,-20,0,0,0,0,0,0,0);
 delay(100);
 }
 
-void girar () {
-    int servos01[] = { 1, 2, 3, 7 };
-    int startAngles01[] = {55, 93, 155, 160};
-    int endAngles01[] = { 25, 123, 160, 155 };
-    smoothMove(4, servos01, startAngles01, endAngles01, t01);
-
-    int servos02[] = { 2, 5, 0, 6};
-    int startAngles02[] = {123, 95, 28, 20};
-    int endAngles02[] = { 143, 90, 35, 90};
-    smoothMove(4, servos02, startAngles02, endAngles02, t01);
-
-    int servos03[] = { 1, 0, 6};
-    int startAngles03[] = {25, 35, 90};
-    int endAngles03[] = { 45, 50, 70};
-    smoothMove(3, servos03, startAngles03, endAngles03, t01);
-
-    int servos04[] = { 2, 0, 3, 6};
-    int startAngles04[] = {143, 50, 160, 70};
-    int endAngles04[] = { 133, 50, 165, 50};
-    smoothMove(4, servos04, startAngles04, endAngles04, 15);
-
-    int servos05[] =      {2, 0, 1, 5, 3, 7, 6};
-    int startAngles05[] = {123, 50, 45, 90, 165, 155, 50};
-    int endAngles05[] =   {93, 28, 50, 95, 155, 160, 20};
-    smoothMove(7, servos05, startAngles05, endAngles05, 15);
-}
 //cinematica
 void updateServoPos(int target1, int target2, int target3, char leg){
   if (leg == 'l'){
-    AdvMoveAbs(20,10,P0,P1,P2,posiciones[3]-(target3-80),posiciones[4]+ target2,posiciones[5]+target1,P6,P7,P8,P9,P10,P11,P12,P13);
+    AdvMoveAbs(t01,10,P0,P1,P2,posiciones[3]-(target3-90-k01),posiciones[4]+ target2+k02,posiciones[5]+target1+k03,P6,P7,P8,P9,P10,P11,P12,P13);
+
   }
   else if (leg == 'r'){ 
-    AdvMoveAbs(20,10, posiciones[0]+(target3-80), posiciones[1]-target2,posiciones[2]-target1,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13);
+    AdvMoveAbs(t01,10, posiciones[0]+(target3-90-k01), posiciones[1]-target2-k02,posiciones[2]-target1-k03,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13);
     
   }
 }
@@ -485,7 +400,7 @@ void pos(float x, float z, char leg){
   float hipDeg = hipDeg1 + hipDeg2;
   float kneeDeg = kneeRad * (180/PI);
   float ankleDeg = ankleRad * (180/PI);
-
+/*
   Serial.print(leg);
   Serial.print("\t");
   Serial.print(ankleDeg);
@@ -494,7 +409,7 @@ void pos(float x, float z, char leg){
   Serial.print("\t");
   Serial.print(hipDeg);
   Serial.print("\t");
-
+*/
   updateServoPos(hipDeg, kneeDeg, ankleDeg, leg);  
 }
 
